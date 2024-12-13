ba0b9218e8a0a9c8e346ba71349681bbee832d7f
suazael <suazael@o2.pl>
2024-12-13 12:59:28 +0100

    Add project files.

---
+++ P02CancellingTasks/Program.cs
@@ -0,0 +1,132 @@
+﻿class Example1
+{
+
+    
+
+    public static void Main1(string[] args)
+    { 
+        var t = new Task(() =>
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine($"{i++}\t");
+            }
+        });
+
+        t.Start();
+
+        Console.WriteLine("Press any key to cancel the task");
+        Console.ReadKey();
+    }
+
+
+}
+
+
+class Example2
+{
+    public static void Main2(string[] args)
+    {
+        var cts = new CancellationTokenSource();
+        var token = cts.Token;
+
+
+        var t = new Task(() =>
+        {
+            int i = 0;
+            while (true)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Task cancelled");
+                    break;
+                }
+                Console.WriteLine($"{i++}\t");
+            }
+        }, token);
+
+        t.Start();
+
+        Console.ReadKey();
+        cts.Cancel();
+
+        Console.WriteLine("Press any key to cancel the task");
+        Console.ReadKey();
+    }
+    //https://github.com/tomasz-trener/ParallelProgrammingDay1
+
+}
+
+class Example3
+{
+    public static void Main3(string[] args)
+    {
+        var cts = new CancellationTokenSource();
+        var token = cts.Token;
+
+
+        var t = new Task(() =>
+        {
+            int i = 0;
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
+                //if (token.IsCancellationRequested)
+                //{
+                //    throw new OperationCanceledException(token);
+                //}
+                Console.WriteLine($"{i++}\t");
+            }
+        }, token);
+
+        t.Start();
+
+        Console.ReadKey();
+        cts.Cancel();
+
+        Console.WriteLine("Press any key to cancel the task");
+        Console.ReadKey();
+    }
+    //https://github.com/tomasz-trener/ParallelProgrammingDay1
+
+}
+
+class Example4
+{
+    public static void Main(string[] args)
+    {
+        var cts = new CancellationTokenSource();
+        var token = cts.Token;
+
+        token.Register(() =>
+        {
+            Console.WriteLine("Cancellation requested");
+          //  token.ThrowIfCancellationRequested(); // unikamy takiego podejscia
+        });
+
+        var t = new Task(() =>
+        {
+            int i = 0;
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
+                //if (token.IsCancellationRequested)
+                //{
+                //    throw new OperationCanceledException(token);
+                //}
+                Console.WriteLine($"{i++}\t");
+            }
+        }, token);
+
+        t.Start();
+
+        Console.ReadKey();
+        cts.Cancel();
+
+        Console.WriteLine("Press any key to cancel the task");
+        Console.ReadKey();
+    }
+    //https://github.com/tomasz-trener/ParallelProgrammingDay1
+
+}
---
+++ P08SpinLocking/Program.cs
@@ -0,0 +1,112 @@
+﻿class Example1
+{
+    public class BankAccount
+    {
+        public object padlock = new object();
+
+        public int Balance { get => _balance; private set => _balance = value; }
+
+        private int _balance;
+
+        public void Deposit(int amount)
+        {
+            var lockTaken = false;
+            try
+            {
+                sl.Enter(ref lockTaken);
+                _balance += amount;
+            }
+            finally
+            {
+                if (lockTaken) sl.Exit();
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            var lockTaken = false;
+            try
+            {
+                sl.Enter(ref lockTaken);
+                _balance -= amount;
+            }
+            finally
+            {
+                if (lockTaken) sl.Exit();
+            }
+        }
+
+        SpinLock sl = new SpinLock();
+
+        public static void Main1(string[] args)
+        {
+            var tasks = new List<Task>();
+            var ba = new BankAccount();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        ba.Deposit(100);
+                    }
+                }));
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        ba.Withdraw(100);
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"Final balance is {ba.Balance}");
+
+        }
+
+    }
+}
+
+
+class Example2
+{
+    static SpinLock sl = new SpinLock();
+    public static void LockRecursion(int x)
+    {
+        bool lockTaken = false; // zmienna informująca czy Spinlock jest zajęty
+
+        try
+        {
+            sl.Enter(ref lockTaken); // próba zajęcia Spinlocka
+        }
+        catch (LockRecursionException e)
+        {
+            Console.WriteLine("Error: " + e);
+        }
+        finally
+        {
+            if(lockTaken)
+            {
+                Console.WriteLine($"zdobyto x={x}");
+
+                if (x > 0)
+                {
+                    LockRecursion(x - 1);
+                }
+                sl.Exit();
+            }
+            else
+            {
+                Console.WriteLine($"Cant access x={x}");
+            }
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        LockRecursion(5);
+    }
+}
